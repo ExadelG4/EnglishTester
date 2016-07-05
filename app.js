b52d44e34943d39e4b55844affa2913e0f7f1722
egri4 <grachstk@gmail.com>
2016-07-05 21:35:47 +0300

    mini-test server

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
+
 var express = require('express');
 var app = express();
+var path = require("path");
+var MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/test', function(err, db){
+  if (err) {
+    throw err;
+  }
+  
+    var temp = {name: 'azaFSDFSDFza', desc: 'ADASDAS', phone: '5455555'};
+    db.collection('c1').insert(temp);
+    db.collection('c1').find().toArray(function(err, result) {
+    if (err) {
+      throw err;
+    }
+    console.log(result);
+  });
+});
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname+'/index.html'));
+
+});
+app.post('/', function (req, res) {
+	
+	console.log(JSON.stringify(req));
+  res.sendFile(path.join(__dirname+'/index.html'));
+
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000 !');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
