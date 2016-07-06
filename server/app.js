af2fd5f1b2d2f139c8c69c89b6e5a621c820df81
YakushevSV <cem3@mail.ru>
2016-07-06 17:55:36 +0300

    start router

--- server/app.js
+++ server/app.js
@@ -1,22 +1,13 @@
 
 var express = require('express');
 var app = express();
-var path = require("path");
 var mongo = require('./db/mongo');
 
-app.get('/', 
-	function (req, res, next) {
-		mongo.user.find().then(function(data){
-			console.log(data);
 
-			next();
-		});
+var router = require('./route');
 
-	  res.sendFile(path.join(__dirname+'/index.html'));
-	}, 
-	function (req, res) {
-		console.log(1230);
-	});
+app.use('/', router);
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000 !');
