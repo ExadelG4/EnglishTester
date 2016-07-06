a6aa9d92a6c225dda4bd70c851bb7f4422e21b3d
egri4 <grachstk@gmail.com>
2016-07-06 16:19:01 +0300

    del testing, new structure

---
+++ server/db/schemes/user.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+var UserSchema = new Schema(
+	{
+  		_id: String,
+  		name: String,
+  		pass: String
+	});
+	
+var user = mongoose.model('users', UserSchema);
+
+module.exports = user;
