d42906737531ebdb3a628772dc073f26c5dabc38
YakushevSV <cem3@mail.ru>
2016-07-07 19:05:45 +0300

    layer between server and schemes

--- server/services/userService.js
+++ server/services/userService.js
@@ -1,9 +1,7 @@
 var mongo = require('../db/mongo');
-function getAll(){
-	return mongo.user.find(function (err, array) {
- 		//if (err) return throw new Error(err);
-
-		return array; 	 
-	})
+function getAllUsers(){
+	return mongo.user.find();
 }
-module.exports.getAll = getAll;
+
+
+module.exports.getAllUsers = getAllUsers;
