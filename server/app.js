4d05a9978159e6a53e877401e27f39612a59e210
egri4 <grachstk@gmail.com>
2016-07-07 14:13:23 +0300

    try to setUp server

--- server/app.js
+++ server/app.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var app = express();
 var mongo = require('./db/mongo');
-
-
+var path = require('path');
+var bodyParser = require('body-parser');
 var router = require('./route');
 
+var rootDir = __dirname.substring(0, __dirname.lastIndexOf('\\'));
+
+app.use(express.static(rootDir + '\\client'));
+app.use(express.static(rootDir + '\\bower_components'));
+app.use(express.static(rootDir + '\\node_modules'));
 app.use('/', router);
+// app.use(express.static(__dirname + '/../client'));
+// app.use(express.static(__dirname + '/../bower_components/angular/'));
+// app.use(express.static(__dirname + '/../node_modules'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.get('*', function(req, res) {
+	//res.set('content-type','text/html')
+	res.sendFile(path.join(__dirname+'/../client/index.html'));
+});
 
 
 app.listen(3000, function () {
--- server/route.js
+++ server/route.js
@@ -4,9 +4,14 @@ var service = require('./services/userService');
 var path = require("path");
 var router = express.Router();
 
-router.get('/', function(req, res) {
-	  service.print();
-	  res.sendFile(path.join(__dirname+'/index.html'));
+router.get('/getAll', function(req, res) {
+	service.getAll().then(function(data){
+		res.send(data);
+	}).catch(function(e){
+		console.log(e);
+		res.send({error: e});
+	});
+	 // res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 router.post('/',function(req, res) {
