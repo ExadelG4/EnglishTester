054266ae23de171a0009991585f17af79ac31c25
YakushevSV <cem3@mail.ru>
2016-07-07 15:27:07 +0300

    chenge express.static

--- server/app.js
+++ server/app.js
@@ -5,20 +5,10 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var router = require('./route');
 
-var rootDir = __dirname.substring(0, __dirname.lastIndexOf('\\'));
 
-app.use(express.static(rootDir+'\\client'));
-console.log(rootDir);
-app.use(express.static(rootDir));
-//app.use(express.static(rootDir + '\\node_modules'));
-//app.use('/', router);
-// app.use(express.static(__dirname + '/../client'));
-// app.use(express.static(__dirname + '/../bower_components/angular/'));
-// app.use(express.static(__dirname + '/../node_modules'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/../client'));
+app.use(express.static(__dirname + '/../'));
 app.get('*', function(req, res) {
-	//res.set('content-type','text/html')
 	res.sendFile(path.join(__dirname+'/../client/index.html'));
 });
 
