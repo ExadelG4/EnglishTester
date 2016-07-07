882519bb49519a86896caf58cd2d626325207787
Lada051995 <mladda@mail.ru>
2016-07-08 01:13:13 +0300

    fixed responsive header and gitignore

--- client/app.js
+++ client/app.js
@@ -1,4 +1,19 @@
-var app = angular.module("myApp", ['ui.bootstrap']);
+var app = angular.module("myApp", ['ui.bootstrap', 'ui.router']);
 app.controller('myController', ['$scope', '$html', function($scope, $html) {
     $scope.name = 'Vas';
 }]);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise("/home");
+
+    $stateProvider
+        .state('news', {
+            url: "/news",
+            templateUrl: "common/header/news.html"
+        })
+        .state('check', {
+            url: "/check",
+            templateUrl: "common/header/check.html"
+        });
+});
