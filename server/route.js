10529942eec5f912f97b3ba2209dc5cfd6d90a4c
YakushevSV <cem3@mail.ru>
2016-07-07 11:11:01 +0300

    chenge rout

--- server/route.js
+++ server/route.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var contracts = require('./contracts');
 var service = require('./services/userService');
-var path = require("path");
 var router = express.Router();
 
 router.get('/', function(req, res) {
-	  service.print();
-	  res.sendFile(path.join(__dirname+'/index.html'));
+	  
 });
 
 router.post('/',function(req, res) {
 
 });
+
+
 
 module.exports = router;
