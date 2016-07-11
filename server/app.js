b64541e3bed5b22d73c62119603deaadaa7dd11e
YakushevSV <cem3@mail.ru>
2016-07-11 13:45:23 +0300

    fix error after merging

--- server/app.js
+++ server/app.js
@@ -14,6 +14,7 @@ app.use('/',router);
 
 app.get('*', function(req, res) {  
 	res.sendFile(path.join(__dirname + '/../client/index.html'));
+});
 
 
 app.listen(3000, function () {
--- server/services/userService.js
+++ server/services/userService.js
@@ -3,10 +3,10 @@ function getAllUsers(){
 	return user.find();
 }
 function addNewUser(email, pass){
-	return mongo.user.add(email, pass);
+	return user.add(email, pass);
 }
 function authenticate(email, pass){
-	return mongo.user.authenticate(email, pass);
+	return user.authenticate(email, pass);
 }
 
 module.exports.getAllUsers = getAllUsers;
