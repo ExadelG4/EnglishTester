e734361214d2701c828f5753fc39c4c67b571fd9
egri4 <grachstk@gmail.com>
2016-07-12 02:04:54 +0300

    modify addUser in DataGen

--- server/dataGen.js
+++ server/dataGen.js
@@ -12,62 +12,28 @@ prompt.get(['Admin', 'User','Teacher','Guest'], function (err, result) {
    console.log(' Guest: ' + result.Guest);
   
    service.removeCollection().then(function(data){
-		// console.log(data);
 	  }).catch(function (err) {
 		 console.log(err);
 	});
+	var addUser = function(_name,_pass,_type,count){
+			for(i =0; i<count; i++){
+			service.addNewUser2({
+									email: _name+(i+1)+"@exadel.com", 
+								  	pass: _pass+(i+1),
+								  	type: _type,
+								  	name: _name+(i+1)
+								  					}).then(function(data){
+			
+				}).catch(function(err){
+					console.log(err);
+				});
 
-	for(i =0; i<result.Admin; i++){
-		service.addNewUser2({
-								email: "admin"+(i+1)+"@exadel.com", 
-							  	pass: "apass"+(i+1),
-							  	type: "Admin",
-							  	name: "admin"+(i+1)
-							  					}).then(function(data){
-			//	console.log("Add" + data);
-			}).catch(function(err){
-				console.log(err);
-			});
-
+			}
 	}
-	for(i =0; i<result.User; i++){
-		service.addNewUser2({
-								email: "user"+(i+1)+"@exadel.com", 
-							  	pass: "upass"+(i+1),
-							  	type: "User",
-							  	name: "user"+(i+1)
-							  					}).then(function(data){
-			//	console.log("Add" + data);
-			}).catch(function(err){
-				console.log(err);
-			});
-
-	}
-	for(i =0; i<result.Teacher; i++){
-		service.addNewUser2({
-								email: "teacher"+(i+1)+"@exadel.com", 
-							  	pass: "tpass"+(i+1),
-							  	type: "Teacher",
-							  	name: "teacher"+(i+1)
-							  					}).then(function(data){
-			//	console.log("Add" + data);
-			}).catch(function(err){
-				console.log(err);
-			});
-
-	}
-	for(i =0; i<result.Guest; i++){
-		service.addNewUser2({
-								email: "guest"+(i+1)+"@gmail.com", 
-							  	pass: "gpass"+(i+1),
-							  	type: "Guest",
-							  	name: "guest"+(i+1)
-							  					}).then(function(data){
-			//	console.log("Add" + data);
-			}).catch(function(err){
-				console.log(err);
-			});
-
-	}
+	addUser("admin","apass","Admin",result.Admin);
+	addUser("user","upass","User",result.User);
+	addUser("teacher","tpass","Teacher",result.Teacher);
+	addUser("guest","gpass","Guest",result.Guest);
+	
 });
 
