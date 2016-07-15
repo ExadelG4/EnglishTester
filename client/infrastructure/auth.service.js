72e3e839fb904b7964658a69adce14b72f9a709a
Anton Bely <toshabely@gmail.com>
2016-07-15 13:57:10 +0300

    Prepare to get date in ms and check refreshToken

---
+++ client/infrastructure/auth.service.js
@@ -0,0 +1,54 @@
+(function () {
+    'use strict';
+
+    angular.module('infrastructure', [])
+        .factory('authService', ['$http', 'context', '$location',
+            function($http, context, $location) {
+                function refresh(localContext) {
+                    return $http.get('http://localhost:3000/refresh',
+                        {headers: {'Authorization': localContext.token, 'Refresh': localContext.refreshToken}})
+                        .then(function (result) {
+                            localContext.token = result.data.token;
+                            localContext.refreshToken = result.data.refreshToken;
+                            localContext.expiredTime = result.data.expiredTime;
+                        });
+                }
+
+                function checkRefresh() {
+                    var intervalID = setInterval(function () {
+                        var localContext = JSON.parse(localStorage.getItem('context'));
+                        if (localContext) {
+                            var expTime = localContext.expiredTime.getTime();
+                            var now = (new Date()).getTime();
+                            if (expTime > now) {
+                                if (expTime - now < 120 * 1000) {
+                                    refresh(localContext);
+                                }
+                            } else {
+                                clearInterval(intervalID);
+                                $location.path('/login');
+                            }
+                        }
+                    }, 5 * 1000);
+                }
+
+                return {
+                    isAuthenticate: false,
+                    init: function () {
+                        var localContext = JSON.parse(localStorage.getItem('context'));
+                        if (localContext) {
+                            //todo: get time on UTC datetime
+                            if(localContext.expiredTime.getTime() > new Date().getTime()) {
+                                this.isAuthenticated = false;
+                            } else {
+                                checkRefresh();
+                                context.init(localContext.user);
+                                this.isAuthenticate = true;
+                            }
+                        } else {
+                            this.isAuthenticated = false;
+                        }
+                    }
+                };
+    }]);
+})();
