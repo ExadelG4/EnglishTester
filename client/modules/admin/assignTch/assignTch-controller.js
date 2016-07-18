2173284cf102dc764f4624adf8222eaa4057ae88
Lada051995 <mladda@mail.ru>
2016-07-18 12:52:45 +0300

    modify header and adding assign teacher in admin tab

--- client/modules/admin/assignTch/assignTch-controller.js
+++ client/modules/admin/assignTch/assignTch-controller.js
@@ -1,3 +1,29 @@
-/**
- * Created by Yoga 3 Pro on 12.07.2016.
- */
+(function(){
+    angular.module('admin').controller('assignTchController', ['$scope', 'userService', function($scope, userService) {
+        $scope.teachers = [];
+        userService.getTeachers().then(function(data) {
+            data.forEach(function(item, i){
+                $scope.teachers[i] = item;
+                console.log($scope.teachers[i]);
+            });
+        });
+
+        $scope.students = [];
+        userService.getUsers().then(function(data) {
+            data.forEach(function(item, i) {
+                $scope.students[i] = item;
+            });
+        });
+
+        $scope.chooseTeacher = function(item) {
+            $scope.tchName = item.firstName + ' ' + item.lastName;
+            $scope.tchMail = item.email;
+        }
+
+        $scope.chooseUser = function(item) {
+            $scope.stdName = item.firstName + ' ' + item.lastName;
+            $scope.stdMail = item.email;
+        }
+
+    }]);
+})();
