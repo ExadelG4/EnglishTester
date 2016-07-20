aeedb1f1f493315c5c26f60ea4cc1e84e3da59cf
egri4 <grachstk@gmail.com>
2016-07-21 00:31:53 +0300

    add testGen

--- server/services/testService.js
+++ server/services/testService.js
@@ -3,7 +3,19 @@ var testB = require('../db/mongo').testB;
 
 
 function getAllQuestions(){
-	return testA.find();
+	return testA.find({},{},{});
+}
+function getQFromLevel(_level,rand){
+	return testA.find({level: _level},{'_id':1,'type': 1, 'question':1, 'options':1, 'answers': 0},{skip : rand, limit : 1 });
+}
+function addNewQuestion(info){
+	return testA.save(info);	
+}
+function removeCollection(){
+	return testA.remove();
 }
 
 module.exports.getAllQuestions = getAllQuestions;
+module.exports.getQFromLevel = getQFromLevel;
+module.exports.addNewQuestion = addNewQuestion;
+module.exports.removeCollection = removeCollection;
