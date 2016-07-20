e6dbecb0a8045e21926261fdd25c090b16b4df68
Lada051995 <mladda@mail.ru>
2016-07-20 13:52:19 +0300

    modify routing

--- client/route.js
+++ client/route.js
@@ -24,28 +24,28 @@
                     controller: 'adminController',
                     role: ['admin']
                 })
-                .state('tests', {
-                    url: '/tests',
-                    templateUrl: 'modules/tests/tests.html',
-                    controller: 'testsController',
-                    role: ['user', 'guest']
-                })
-                .state('statistic', {
-                    url: '/statistic',
-                    templateUrl: 'modules/statistic/statistic.html',
-                    controller: 'statisticController'
-                })
-                .state('userHome', {
-                    url: '/userHome',
-                    templateUrl: 'modules/userHome/userHome.html',
-                    controller: 'userHomeController'
-                })
-                .state('passTestPart1', {
-                    url: '/passTestPart1',
-                    templateUrl: 'modules/userHome/passTest/passTestPart1.html',
-                    controller: 'passTestController'
-
-                })
+                //.state('tests', {
+                //    url: '/tests',
+                //    templateUrl: 'modules/tests/tests.html',
+                //    controller: 'testsController',
+                //    role: ['user', 'guest']
+                //})
+                //.state('statistic', {
+                //    url: '/statistic',
+                //    templateUrl: 'modules/statistic/statistic.html',
+                //    controller: 'statisticController'
+                //})
+                //.state('userHome', {
+                //    url: '/userHome',
+                //    templateUrl: 'modules/userHome/userHome.html',
+                //    controller: 'userHomeController'
+                //})
+                //.state('passTestPart1', {
+                //    url: '/passTestPart1',
+                //    templateUrl: 'modules/userHome/passTest/passTestPart1.html',
+                //    controller: 'passTestController'
+                //
+                //})
         }
     );
 })();
