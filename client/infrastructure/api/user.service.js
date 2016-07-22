62d982cf9b8ca728d1b7a6b15970670e1abc180a
Lada051995 <mladda@mail.ru>
2016-07-22 12:28:15 +0300

    adding post request studentsAssign

--- client/infrastructure/api/user.service.js
+++ client/infrastructure/api/user.service.js
@@ -59,6 +59,10 @@
                                 return false;
                              }
                         );
+                    },
+
+                    assignStudents: function(list) {
+                        return httpService.post('http://localhost:3000/assignTest', list);
                     }
                 };
             }]);
