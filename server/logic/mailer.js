ccb9f521c1c2f2f84924aa9ad4769d31e28bad59
ExadelG4 <grachstk@gmail.com>
2016-07-27 13:07:20 +0300

    add mailer

--- server/logic/mailer.js
+++ server/logic/mailer.js
@@ -1,34 +1,38 @@
-var nodemailer = require('nodemailer');
-var smtpTransport = require('nodemailer-smtp-transport');
 
-var mailAccountUser = 'grachstk@gmail.com'
-var mailAccountPassword = 'kpacubo2016!'
+var nodemailer = require("nodemailer");
+var smtpTransport = require("nodemailer-smtp-transport");
 
-var fromEmailAddress = 'grachstk@gmail.com'
-var toEmailAddress = 'egri4stk@gmail.com'
 
-var transport = nodemailer.createTransport(smtpTransport({
-    service: 'gmail',
-    auth: {
-        user: mailAccountUser,
-        pass: mailAccountPassword
+var smtpTransport = nodemailer.createTransport(smtpTransport({
+    host : 'smtp.gmail.com',
+    secureConnection : false,
+    port: 465,
+    auth : {
+        user : 'grachstk@gmail.com',
+        pass : 'kpacubo2016!'
+    },
+    tls: {
+        rejectUnauthorized: false
     }
-}))
+}));
 
-var mail = {
-    from: fromEmailAddress,
-    to: toEmailAddress,
-    subject: "hello world!",
-    text: "Hello!",
-    html: "<b>Hello!</b><p><a href=\"http://www.yahoo.com\">Click Here</a></p>"
-}
+var mailOptions={
+        from : 'grachstk@gmail.com',
+        to : 'egri4stk@gmail.com',
+        subject : 'Your Subject',
+        text : 'Your Text',
+        html : 'HTML GENERATED'
+    };
 
-transport.sendMail(mail, function(error, response){
-    if(error){
-        console.log(error);
-    }else{
-        console.log("Message sent: " + response.message);
-    }
+        smtpTransport.sendMail(mailOptions, function(error, response){
+            if(error){
+                console.log(error);
+                
+            }else{
+                console.log(response.response.toString());
+                console.log("Message sent: " + response.message);
+               
+            }
+        });
+   
 
-    transport.close();
-});
