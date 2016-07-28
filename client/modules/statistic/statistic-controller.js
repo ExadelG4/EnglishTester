a68c3eda894d696d872b2003eea6f60427ac1956
Lada051995 <mladda@mail.ru>
2016-07-28 15:00:28 +0300

    adding post request on statistics

--- client/modules/statistic/statistic-controller.js
+++ client/modules/statistic/statistic-controller.js
@@ -3,11 +3,16 @@
 
     angular.module('statistic', []).controller('statisticController', ['$scope', '$state', 'userService', function($scope, $state, userService) {
 
+        $scope.searchList = [];
 
         $scope.searchUser = function() {
+            $scope.searchList = [];
             if ($scope.searching.length >= 3) {
                 userService.searchUser($scope.searching).then(function(data) {
-                    console.log(data);
+                    data.forEach(function(item, i) {
+                        $scope.searchList[i] = item;
+                        $scope.searchList[i].fullName = item.firstName + ' ' + item.lastName;
+                    });
                 });
             }
         };
