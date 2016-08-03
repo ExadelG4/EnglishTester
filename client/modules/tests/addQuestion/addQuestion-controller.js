68149f4abcdf7c59319d28fd8b829d242f6cfb0a
YakushevSV <cem3@mail.ru>
2016-08-03 13:34:20 +0300

    fix file upload

--- client/modules/tests/addQuestion/addQuestion-controller.js
+++ client/modules/tests/addQuestion/addQuestion-controller.js
@@ -115,4 +115,89 @@
 				$scope.selectedQue = '';
 				notification.success("You have successfully added new question");
 			};
-}])})();
+
+
+
+
+
+
+
+			$scope.fileNameChanged = function (ele) {
+				var files = ele.files;
+				var l = files.length;
+				var namesArr = [];
+
+				for (var i = 0; i < l; i++) {
+					namesArr.push(files[i].name);
+				}
+			}
+
+
+
+
+			$scope.submit = function(){
+				// var file = this.elements.myfile.files[0];
+				var file = $scope.vm.uploadme;
+				if (file) {
+					upload(file);
+				}
+				return false;
+			};
+
+			function upload(file) {
+
+				var xhr = new XMLHttpRequest();
+
+				// обработчики можно объединить в один,
+				// если status == 200, то это успех, иначе ошибка
+				xhr.onload = xhr.onerror = function () {
+					if (this.status == 200) {
+						log("success");
+					} else {
+						log("error " + this.status);
+					}
+				};
+
+				// обработчик для закачки
+				xhr.upload.onprogress = function (event) {
+					log(event.loaded + ' / ' + event.total);
+				}
+
+				xhr.open("POST", "upload", true);
+				xhr.send(file);
+
+			}
+
+
+			function log(html) {
+				document.getElementById('log').innerHTML = html;
+			}
+
+
+
+
+}]).directive("fileread", [function () {
+    return {
+        scope: {
+            fileread: "="
+        },
+        link: function (scope, element, attributes) {
+            element.bind("change", function (changeEvent) {
+                // var reader = new FileReader();
+                // reader.onload = function (loadEvent) {
+                //     scope.$apply(function () {
+                //         scope.fileread = loadEvent.target.result;
+                //     });
+                // }
+                // reader.readAsDataURL(changeEvent.target.files[0]);
+				scope.fileread = changeEvent.target.files[0];
+            });
+        }
+    }
+
+	// return {
+	// 	changeEvent.target.files[0]
+	// }
+
+}])
+})();
