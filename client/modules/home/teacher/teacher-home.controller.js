f83070eddfc2e0416ff3c4aaca7be98cf75393b9
DashaRyabtseva <dasha.ryabtseva47@gmail.com>
2016-08-03 19:50:47 +0300

    teacher-home, almost fixed addque

--- client/modules/home/teacher/teacher-home.controller.js
+++ client/modules/home/teacher/teacher-home.controller.js
@@ -4,22 +4,18 @@
     angular.module('home')
         .controller('teacherHomeController', ['$scope', 'userService',
             function($scope, userService) {
-                $scope.list = []
+
+                $scope.list = [];
+                $scope.date = [];
 
                 userService.getTestsList()
                     .then ( function(data) {
                         $scope.list = data;
+                        for (var i = 0; i < data.length; ++i) {
+                            $scope.date[i] = new Date($scope.list[i].date).toDateString();
+                        }
                     })
                 
-                $scope.testList = [
-                    {
-                        level: 'Intermediate',
-                        date: new Date()
-                    },
-                    {
-                        level: 'Upper-Intermediate',
-                        date: new Date()
-                    }
-                ];
+
             }]);
 })();
